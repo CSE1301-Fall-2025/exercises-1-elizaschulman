a4b0813d5da6df6530f9fdc92d50920aa4b9db85
elizaschulman <e.r.schulman@wustl.edu>
2025-09-13 22:10:39 -0500

    done

--- src/module2/_01ifs/E03TestNumbers.java
+++ src/module2/_01ifs/E03TestNumbers.java
@@ -17,5 +17,10 @@ public class E03TestNumbers {
 		if (number == 0) {
 			System.out.println(number + " is zero!");
 		}
+		if (number < 0) {
+			System.out.println (number + " is negative!");
+		}
+	
 	}
+
 }
